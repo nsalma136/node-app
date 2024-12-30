dd5c9dba72358b09ecf2eaa66e9ad7f41d6f923a
nsalma136 <nsalma006@gmail.com>
2024-12-29 21:24:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(express.json());
 // Basic GET endpoint with a cooler response
 app.get('/', (req, res) => {
     const coolResponses = [
-        "🚀 Welcome to the Node.js Express server, where coding dreams come true check redeploy!",
-        "✨ You've just landed in the realm of awesome Express servers after check redeploy!",
-        "🌟 Hello there, explorer! Ready to embark on a Node.js adventure check redeploy?"
+        "🚀 Welcome to the Node.js Express server, where coding dreams come true check test team!",
+        "✨ You've just landed in the realm of awesome Express servers after check test team!",
+        "🌟 Hello there, explorer! Ready to embark on a Node.js adventure check test team?"
     ];
     const randomIndex = Math.floor(Math.random() * coolResponses.length);
     console.log("GET / endpoint called");
